385b33a99d33bfe23166be19d9ec23dc194aa501
Vladimir Ledyaev <vl.ledyaev@gmail.com>
2017-03-05 22:48:16 +0300

    Add saving messages to the database.

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -9,7 +9,6 @@ import { MessageService } from "./message.service";
         <div class="col-md-8 col-md-offset-2">
             <app-message
                    [message]="message"
-                   (editClicked)="message.content = $event"
                     *ngFor="let message of messages"></app-message>
         </div>
     `
@@ -20,6 +19,11 @@ export class MessageListComponent implements OnInit {
     constructor(private messageService: MessageService) {}
 
     ngOnInit() {
-        this.messages = this.messageService.getMessages();
+        this.messageService.getMessages()
+            .subscribe(
+                (messages: Message[]) => {
+                    this.messages = messages;
+                }
+            );
     }
 }
