05078982f44e193344fd1418aaeb659619e3c391
Vladimir Ledyaev <vl.ledyaev@gmail.com>
2017-03-08 19:22:18 +0300

    MongoDb configuration.

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    var user = new User({
+        firstName: 'Vladimir',
+        lastName: 'Ledyaev',
+        password: 'pass',
+        email: 'ledyaev@tut.by'
+    });
+    user.save();
+    User.findOne({}, function(err, doc) {
+        if (err) {
+            return res.send('Error!');
+        }
+        res.render('node', {email: doc.email});
+    });
 });
 
-router.get('/message/:msg', function (req, res, next) {
-    res.render('node', {message: req.params.msg});
-});
-
-router.post('/message', function(req, res, next) {
-    var message = req.body.message;
-    res.redirect('/message/' + message);
+router.get('/1', function(req, res, next) {
+    var user = new User({
+        firstName: 'Vladimir',
+        lastName: 'Ledyaev',
+        password: 'pass',
+        email: 'ledyaev@tut.by'
+    });
+    user.save();
+    res.redirect('/');
 });
 
 module.exports = router;
