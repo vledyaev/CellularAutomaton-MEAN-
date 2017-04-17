8efbdb26b34d036a865c13489437a0a872d4a4e4
Vladimir Ledyaev <vl.ledyaev@gmail.com>
2017-04-18 00:26:58 +0300

    Add angular components and angular routing.

--- routes/app.js
+++ routes/app.js
@@ -1,32 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    var user = new User({
-        firstName: 'Vladimir',
-        lastName: 'Ledyaev',
-        password: 'pass',
-        email: 'ledyaev@tut.by'
-    });
-    user.save();
-    User.findOne({}, function(err, doc) {
-        if (err) {
-            return res.send('Error!');
-        }
-        res.render('node', {email: doc.email});
-    });
-});
-
-router.get('/1', function(req, res, next) {
-    var user = new User({
-        firstName: 'Vladimir',
-        lastName: 'Ledyaev',
-        password: 'pass',
-        email: 'ledyaev@tut.by'
-    });
-    user.save();
-    res.redirect('/');
+    res.render('index');
 });
 
 module.exports = router;
