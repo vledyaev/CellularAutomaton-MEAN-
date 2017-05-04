8d7ddf94ee78434db27ece4199e7498d25cc64f3
Vladimir Ledyaev <vl.ledyaev@gmail.com>
2017-05-04 21:37:09 +0300

    Add exception logging

--- app.js
+++ app.js
@@ -47,5 +47,10 @@ app.use(function (req, res, next) {
     }
 });
 
+process.on('uncaughtException', function (err) {
+    console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
+    console.error(err.stack)
+    process.exit(1)
+})
 
 module.exports = app;
